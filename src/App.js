10e55a9aa23fbcd6d92f89675bbba39a7197a90b
Akash Mishra <mernjsstack@gmail.com>
2020-10-16 21:30:10 +0530

    Menu Item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Homepage from "./Component/Homepage/home";
+import Homepage from "./page/Homepage/home";
+import { Route, Switch } from "react-router-dom";
+import HomePage from './page/Homepage/home';
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component = {HomePage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/page/Homepage/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.scss";
+import DirectoryMenu from "../../Component/directory/directory";
+
+const HomePage = () => (
+        <div className='homepage'>
+            <DirectoryMenu/>
+        </div>
+)
+
+export default HomePage
